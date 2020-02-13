52de1dc74387cebe8cc836ba912d57fa80f63a93
Leonardo Santana <hi@leonsantana.com>
2020-02-12 21:44:11 -0500

    added inital user table and data structure, init the migration into the db

---
+++ db/migrate/20200212235626_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+
+      t.string :name
+      t.string :user_image
+      t.string :country
+      t.string :user_spotify_url
+      t.string :spotify_id
+      t.string :access_token
+      t.string :refresh_token
+
+      t.timestamps
+    end
+  end
+end
