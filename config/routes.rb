214b3db750b0203a9991ba80f0d43ed54360a1d7
Leonardo Santana <hi@leonsantana.com>
2020-02-18 08:48:11 -0500

    configured initial route for auth to spotify

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
+  #Auth 
+  namespace :api do
+    namespace :v1 do
+    
+      get '/login', to: "auth#spotify_request"
+      get '/auth', to: "auth#show"
+      get '/user' to: "user#create"
+    end
+  end
+  
+  resources :users
+
 end
